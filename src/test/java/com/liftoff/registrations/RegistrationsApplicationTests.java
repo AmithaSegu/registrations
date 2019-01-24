e0903382c0b644252a83f1832a0194f35cdf8d4e
Amitha Segu <amitha.segu@gmail.com>
2019-01-24 17:32:45 -0600

    Initial commit

---
+++ src/test/java/com/liftoff/registrations/RegistrationsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.liftoff.registrations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistrationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
